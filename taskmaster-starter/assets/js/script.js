407663f85674e9ed858260cfae49c7250071e7dd
Jimbei <contrerasr2272@gmail.com>
2022-06-03 13:04:13 -0400

    adding Jquery and bootstrap in knowledge

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -45,6 +45,108 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// event and callback function using $, convert "this" into a jQuery object.
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+  .text()
+  .trim();
+  console.log(text);
+
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+
+  //replaces the var text with the var textInput to turn it from a <p> to a <textarea> element.
+$(this).replaceWith(textInput);
+
+// automatically highlights the box to edit text.
+textInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "textarea", function(){
+
+  //get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trim();
+
+  //get the parent ul's id attribute
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  //get the task's position in the list of other li elements
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+tasks[status][index].text = text;
+saveTasks();
+
+//recreate p element to convert the <textarea> back into a <p> element.
+var taskP = $("<p>")
+.addClass("m-1")
+.text(text);
+
+//replace textarea with p element
+$(this).replaceWith(taskP);
+});
+
+//due date was clicked
+$(".list-group").on("click", "span", function(){
+  //get current text
+  var date = $(this)
+  .text()
+  .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+  .attr("type", "text")
+  .addClass("form-control")
+  .val(date);
+
+  //swap out elements
+  $(this).replaceWith(dateInput);
+
+  //automatically focus on the new element
+  dateInput.trigger("focus");
+});
+
+//convert the date from an input back into a span element when someone click outside the input box for date.
+
+//value of due date was changed
+$(".list-group").on("blur", "input[type='text']",function(){
+  //get current text
+  var date = $(this)
+  .val()
+  .trim();
+
+  //get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  //get the task's position in the list of other li elements
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  //update task in array and re-save to localStorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  //recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  //replace input with span element
+  $(this).replaceWith(taskSpan);
+
+});
+
 
 
 
